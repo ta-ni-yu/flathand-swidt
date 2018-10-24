dc497e39614ed8d423e5ad6ff5256bfe6e646777
Yuki Tanimura <yk-tanimura@kddi.com>
2018-10-24 18:09:28 +0900

    hogehog

---
+++ flathand/flathandTests/flathandTests.swift
@@ -0,0 +1,35 @@
+//
+//  flathandTests.swift
+//  flathandTests
+//
+//  Created by yamori-developer on 2018/07/11.
+//  Copyright © 2018年 yamori-developer. All rights reserved.
+//
+
+import XCTest
+@testable import flathand
+
+class flathandTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertEqual(0, 1)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
